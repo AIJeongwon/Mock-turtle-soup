90de5eaa88ce9ec23d12af524f9eb5c028fc67e4
AIJeongwon <pjw5454@kyonggi.ac.kr>
2024-09-13 14:59:08 +0000

    test.py 제거, 가상환경 통합용

--- test.py
+++
@@ -1,8 +0,0 @@
-# single_turn.py
-import google.generativeai as genai
-import os
-#model = genai.GenerativeModel('gemini-pro')
-model = genai.GenerativeModel('gemini-1.5-flash') 
-response = model.generate_content("인공지능에 대해 한 문장으로 설명하세요.")
-
-print(response.text)
