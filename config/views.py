2f05149c14093aafd81bf94f6a3120bc93e7f7cf
Hyun Jin <jinhyun636@gmail.com>
2024-09-22 23:05:24 +0900

    Django 파일 구조 변경

--- config/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django .views.generic import View
-
-# Create your views here.
-def mainpage(request):
-    return render(request, "mainpage.html")
